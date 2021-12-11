16eaf6ba0695df7b285eab94c6e996d098c65f9f
EmanEltokhy <emaneltokhy6@gmail.com>
2021-12-12 00:36:21 +0200

    -loading Data -combining two Datasets

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ df_positive =pd.read_csv("train_Arabic_tweets_positive.tsv", delimiter="\t")
 
 tsv_file = open("AllData.tsv", "a", encoding='utf-8')
 writer = csv.writer(tsv_file, delimiter="\t")
-for i in range(0,len(df_positive)):
+for i in range(len(df_positive)):
     r = list(df_positive.loc[i, :])
     writer.writerow(r)
 for i in range(len(df_negative)):
